254f2c07e5a7f777d4859cf5698df27678a9f395
HafidaHamcherif <58478522+HafidaHamcherif@users.noreply.github.com>
2020-06-16 18:02:02 +0200

    add commit

--- swapCase.js
+++ swapCase.js
@@ -1,6 +1,22 @@
-function swapCase(str){
-    var arr = str.split();
+/*function swapCase(str){
+    var arr = str.toUpperCase();
     console.log(arr);
-    
+    var rev = arr.split('');
+    console.log(rev);
+    var letter = rev[0].toLowerCase();
+    console.log(letter);
+    var letter2 = rev[6].toLowerCase();
+    console.log(letter2);
+    var letter3 = rev[7].toLowerCase();
+    console.log(letter3);
+    var letter4 = rev[8].toLowerCase();
+    console.log(letter4);
 }
-swapCase('hello word');
+swapCase('Hello-LOL');*/
+
+
+var str = 'Hello Word';
+var arr = str.split('')
+var ret = arr.reverse();
+var maj = 
+console.log(ret);
